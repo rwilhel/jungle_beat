f130bd897f14f2b3c5330a3eef4a05d64dd48ef6
rwilhel <rwilhel@gmail.com>
2017-03-23 08:52:58 -0600

    Edits to file.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,13 +1,15 @@
 require 'pry'
 require './lib/node'
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :count
 
   def initialize(head = nil)
     @head = head
+    @count = 0
   end
 
-  def append(data) 
+  def append(data)
+    @count += 1
     if @head.nil?
        @head = Node.new(data)  
       return data  
@@ -18,14 +20,6 @@ class LinkedList
     end
     current_node.next_node = Node.new(data)
     data
-  end
-
-  def count
-    node_count = 0
-    if @head != nil
-      node_count += 1
-    end
-    node_count
   end
 
   def to_string
